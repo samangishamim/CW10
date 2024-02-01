0ab7aa1a0d8a034d2b57e611a2f51e7c33026318
samangishamim <samangi624@gmail.com>
2024-02-01 09:34:09 +0330

    implement methods

--- src/main/java/base/repository/BaseRepositoryImpl.java
+++ src/main/java/base/repository/BaseRepositoryImpl.java
@@ -1,4 +1,27 @@
 package base.repository;
 
-public class BaseRepositoryImpl {
+import base.model.BaseEntity;
+
+import java.io.Serializable;
+
+public class BaseRepositoryImpl <ID extends Serializable,T extends BaseEntity<ID>> implements BaseRepository<ID,T>{
+    @Override
+    public void save(T entity) {
+
+    }
+
+    @Override
+    public T findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public void update(T entity) {
+
+    }
+
+    @Override
+    public void delete(ID id) {
+
+    }
 }
