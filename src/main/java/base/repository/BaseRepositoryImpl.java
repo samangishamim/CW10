c0df4a38d74487e6b5a3ccc69f71d69a643a3a48
samangishamim <samangi624@gmail.com>
2024-02-01 09:46:26 +0330

    implement save method

--- src/main/java/base/repository/BaseRepositoryImpl.java
+++ src/main/java/base/repository/BaseRepositoryImpl.java
@@ -3,10 +3,26 @@ package base.repository;
 import base.model.BaseEntity;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class BaseRepositoryImpl <ID extends Serializable,T extends BaseEntity<ID>> implements BaseRepository<ID,T>{
+public abstract class BaseRepositoryImpl<ID extends Serializable, T extends BaseEntity<ID>>
+        implements BaseRepository<ID, T> {
+    public final Connection connection;
+
+    protected BaseRepositoryImpl(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
-    public void save(T entity) {
+    public void save(T entity) throws SQLException {
+        //todo : INSERT INTO tableName (fieldName) VALUES (QuestionMarks)
+        String sql = "INSERT INTO " + getTableName() + getFieldName() + " VALUES " + getQuestionMarks();
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            setFields(ps);
+            ps.executeUpdate();
+        }
 
     }
 
@@ -24,4 +40,13 @@ public class BaseRepositoryImpl <ID extends Serializable,T extends BaseEntity<ID
     public void delete(ID id) {
 
     }
+
+    public abstract String getTableName();
+
+    public abstract String getQuestionMarks();
+
+    public abstract String getFieldName();
+
+    public abstract String setFields(PreparedStatement ps);
+
 }
