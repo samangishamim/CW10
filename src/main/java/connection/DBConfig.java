d0b745e20163cc8674896190c7dc3d8649e0bb2b
samangishamim <samangi624@gmail.com>
2024-02-01 11:07:06 +0330

    add connection to database

--- src/main/java/connection/DBConfig.java
+++ src/main/java/connection/DBConfig.java
@@ -1,4 +1,25 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConfig {
+
+    private static Connection connection = null;
+    public DBConfig () {}
+
+    public static Connection getConnection()  {
+        if (connection==null){
+            String url="jdbc:postgresql://localhost:5432/cw10";
+            String username="postgres";
+            String password="shamim1379";
+            try {
+                connection= DriverManager.getConnection(url,username,password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
 }
