218211ae3a568b76fa8032a27eb34d8ada37674e
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-01 16:05:13 +0330

    add constructor

--- src/main/java/service/player/PlayerServiceImpl.java
+++ src/main/java/service/player/PlayerServiceImpl.java
@@ -1,4 +1,12 @@
 package service.player;
 
-public class PlayerServiceImpl {
+import base.service.BaseServiceImpl;
+import model.Player;
+import repository.player.PlayerRepository;
+
+public class PlayerServiceImpl extends BaseServiceImpl<Integer , Player , PlayerRepository>
+        implements PlayerService {
+    public PlayerServiceImpl(PlayerRepository repository) {
+        super(repository);
+    }
 }
