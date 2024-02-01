5b99da8006647b9e2a85c1c2f0ff31cd543122c5
samangishamim <samangi624@gmail.com>
2024-02-01 11:34:20 +0330

    add constructor

--- src/main/java/service/city/CityServiceImpl.java
+++ src/main/java/service/city/CityServiceImpl.java
@@ -1,4 +1,11 @@
 package service.city;
 
-public class CityServiceImpl {
+import base.service.BaseServiceImpl;
+import model.City;
+import repository.city.CityRepository;
+
+public class CityServiceImpl extends BaseServiceImpl <Integer,City,CityRepository> implements  CityService{
+    public CityServiceImpl(CityRepository repository) {
+        super(repository);
+    }
 }
