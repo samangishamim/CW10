8d0841b06ee500b4cc15b9f497f9f229aa1e4f2a
samangishamim <samangi624@gmail.com>
2024-02-01 12:01:49 +0330

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,11 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         ApplicationContext.initialize();
-        CityService cityService= ApplicationContext.getCityService();
-        cityService.save(new City("tehran"));
+        CityService cityService = ApplicationContext.getCityService();
+//        cityService.save(new City("tehran"));
 
+//        System.out.println(cityService.findById(1));
+//        cityService.update(new City(1,"TEHRAN"));
+//        cityService.delete(1);
     }
 }
