d4b2e9eb19251082c5574ac30df2fafed343dff8
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-01 15:49:33 +0330

    add needed fields and lombok annotation and needed constructor

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,26 @@
 package model;
 
-public class Player {
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Player extends BaseEntity<Integer> {
+    String playerName ;
+    BigDecimal salary ;
+    int teamId ;
+
+    public Player(int id , String playerName , BigDecimal salary , int teamId){
+        super(id);
+        this.playerName=playerName;
+        this.salary=salary;
+        this.teamId=teamId;
+    }
 }
