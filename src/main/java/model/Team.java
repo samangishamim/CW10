bc41faa0739fcd59772fefbfef263b5cc1878f1e
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-01 14:30:22 +0330

    add needed fields and lombok annotation

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Team {
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Team extends BaseEntity<Integer> {
+
+    String teamName ;
+    int cityId ;
+
 }
