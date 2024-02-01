7457fa9c32f7e273b2d660c43a28d5b1cba2d7de
samangishamim <samangi624@gmail.com>
2024-02-01 16:54:31 +0330

    fix the method

--- src/main/java/repository/player/PlayerRepositoryImpl.java
+++ src/main/java/repository/player/PlayerRepositoryImpl.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class PlayerRepositoryImpl extends BaseRepositoryImpl <Integer , Player>
+public class PlayerRepositoryImpl extends BaseRepositoryImpl<Integer, Player>
         implements PlayerRepository {
     public PlayerRepositoryImpl(Connection connection) {
         super(connection);
@@ -35,9 +35,9 @@ public class PlayerRepositoryImpl extends BaseRepositoryImpl <Integer , Player>
 
     @Override
     public void setFields(PreparedStatement ps, Player entity, boolean isCountOnly) throws SQLException {
-        ps.setString(1,entity.getPlayerName());
-        ps.setBigDecimal(2,entity.getSalary());
-        ps.setInt(3,entity.getTeamId());
+        ps.setString(1, entity.getPlayerName());
+        ps.setBigDecimal(2, entity.getSalary());
+        ps.setInt(3, entity.getTeamId());
     }
 
     @Override
@@ -46,27 +46,19 @@ public class PlayerRepositoryImpl extends BaseRepositoryImpl <Integer , Player>
         String playerName = resultSet.getString(2);
         BigDecimal salary = resultSet.getBigDecimal(3);
         int team_id = resultSet.getInt(4);
-        return new Player(id,playerName,salary,team_id);
+        return new Player(id, playerName, salary, team_id);
     }
 
     @Override
     public ArrayList<Player> listOfPlayer() throws SQLException {
         //todo SELECT  * FROM player ;
-        String sql="SELECT * FROM player";
-        try (PreparedStatement ps= connection.prepareStatement(sql,
-                ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY)){
+        String sql = "SELECT * FROM player";
+        try (PreparedStatement ps = connection.prepareStatement(sql,
+                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
             ResultSet resultSet = ps.executeQuery();
 
-
-//            int counter=0;
-//            while (resultSet.next()) counter++;
-//            Player[] players =new Player[counter];
-//            counter=0;
-//            resultSet.beforeFirst();
-            ArrayList<Player> playerList=new ArrayList<>() ;
-
-            while (resultSet.next()){
-//                players [counter++] = mapResultSetToEntity(resultSet);
+            ArrayList<Player> playerList = new ArrayList<>();
+            while (resultSet.next()) {
                 playerList.add(mapResultSetToEntity(resultSet));
             }
             return playerList;
