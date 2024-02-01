2d3a53eb57fc1a2f975a309ac322c4ac89b632ed
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-01 15:09:52 +0330

    add constructor

--- src/main/java/service/team/TeamServiceImpl.java
+++ src/main/java/service/team/TeamServiceImpl.java
@@ -1,4 +1,12 @@
 package service.team;
 
-public class TeamServiceImpl {
+import base.service.BaseServiceImpl;
+import model.Team;
+import repository.team.TeamRepository;
+
+public class TeamServiceImpl extends BaseServiceImpl<Integer , Team , TeamRepository>
+        implements TeamService {
+    public TeamServiceImpl(TeamRepository repository) {
+        super(repository);
+    }
 }
