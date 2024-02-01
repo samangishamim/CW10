8c14dcfb01b2046f13727f5c3048c58647ebd12b
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-01 15:13:59 +0330

    add team repository and service objects

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -3,8 +3,12 @@ package utility;
 import connection.DBConfig;
 import repository.city.CityRepository;
 import repository.city.CityRepositoryImpl;
+import repository.team.TeamRepository;
+import repository.team.TeamRepositoryImpl;
 import service.city.CityService;
 import service.city.CityServiceImpl;
+import service.team.TeamService;
+import service.team.TeamServiceImpl;
 
 import java.sql.Connection;
 
@@ -12,6 +16,9 @@ public class ApplicationContext {
     private  static Connection CONNECTION;
     private  static CityRepository CITY_REPOSITORY;
     private  static CityService CITY_SERVICE;
+    private static TeamRepository TEAM_REPOSITORY;
+    private static TeamService TEAM_SERVICE;
+
 
     public static void initialize(){
         if (CONNECTION == null){
@@ -21,9 +28,16 @@ public class ApplicationContext {
             CITY_REPOSITORY= new CityRepositoryImpl(CONNECTION);
             CITY_SERVICE=new CityServiceImpl(CITY_REPOSITORY);
         }
+        if (TEAM_REPOSITORY == null){
+            TEAM_REPOSITORY = new TeamRepositoryImpl(CONNECTION);
+            TEAM_SERVICE = new TeamServiceImpl(TEAM_REPOSITORY);
+        }
     }
 
     public  static  CityService getCityService(){
         return CITY_SERVICE;
+    }
+    public static TeamService getTeamService(){
+        return TEAM_SERVICE;
     }
 }
