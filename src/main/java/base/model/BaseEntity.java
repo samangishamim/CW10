cb851720404a197611caaddfebadae82e9e93fef
samangishamim <samangi624@gmail.com>
2024-02-01 09:25:10 +0330

    add lombok annotation

--- src/main/java/base/model/BaseEntity.java
+++ src/main/java/base/model/BaseEntity.java
@@ -1,4 +1,16 @@
 package base.model;
 
-public class BaseEntity {
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class BaseEntity<ID extends Serializable> {
+    ID id;
 }
