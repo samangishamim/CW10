04dd6d985c92201f5e53753db47503ddf02ff85c
samangishamim <samangi624@gmail.com>
2024-02-01 16:54:52 +0330

    test of listOfplayer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import utility.ApplicationContext;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -21,22 +22,27 @@ public class Main {
 //        cityService.delete(1);
 
         // todo : test team
-       // TeamService teamService = ApplicationContext.getTeamService();
-       // teamService.save(new Team("Real" , 1));
-       // teamService.save(new Team("Barcelona" , 1));
-       // System.out.println(teamService.findById(2));
-       // teamService.update(new Team(2,"Real Madrid" , 1));
-       // teamService.delete(3);
+        // TeamService teamService = ApplicationContext.getTeamService();
+        // teamService.save(new Team("Real" , 1));
+        // teamService.save(new Team("Barcelona" , 1));
+        // System.out.println(teamService.findById(2));
+        // teamService.update(new Team(2,"Real Madrid" , 1));
+        // teamService.delete(3);
 
         // todo : test player
         PlayerService playerService = ApplicationContext.getPlayerService();
-        //playerService.save(new Player("farhad" , new BigDecimal("250000"), 2));
-//        playerService.save(new Player("asghar" , new BigDecimal("500000"), 2));
-//        playerService.save(new Player("ali" , new BigDecimal("450000"), 2));
-//        playerService.save(new Player("mohammad" , new BigDecimal("600000"), 2));
-//        playerService.save(new Player("ahmad" , new BigDecimal("550000"), 2));
+//        playerService.save(new Player("farhad" , new BigDecimal("250000"), 1));
+//        playerService.save(new Player("asghar" , new BigDecimal("500000"), 1));
+//        playerService.save(new Player("ali" , new BigDecimal("450000"), 1));
+//        playerService.save(new Player("mohammad" , new BigDecimal("600000"), 1));
+//        playerService.save(new Player("ahmad" , new BigDecimal("550000"), 1));
         //System.out.println(playerService.findById(2));
         //playerService.update(new Player(2 , "asghar" , new BigDecimal("300000") , 2));
         //playerService.delete(2);
+
+        ArrayList<Player> players = playerService.listOfPlayer();
+        for (Player player : players) {
+            System.out.println(player);
+        }
     }
 }
