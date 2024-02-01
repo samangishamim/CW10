e822a21de0b6391dc8a6bfe975687e2b62141020
samangishamim <samangi624@gmail.com>
2024-02-01 10:07:23 +0330

    add exception to methods

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -3,13 +3,14 @@ package base.repository;
 import base.model.BaseEntity;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 
 public interface BaseRepository<ID extends Serializable, T extends BaseEntity<ID>> {
-    void save(T entity);
+    void save(T entity) throws SQLException;
 
-    T findById(ID id);
+    T findById(ID id) throws SQLException;
 
-    void update(T entity);
+    void update(T entity) throws SQLException;
 
-    void delete(ID id);
+    void delete(ID id) throws SQLException;
 }
