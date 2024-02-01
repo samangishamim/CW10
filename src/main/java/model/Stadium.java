4d90318f15ceeb893a9612767c09c0de0ab080a3
samangishamim <samangi624@gmail.com>
2024-02-01 16:59:20 +0330

    add lombok annotation and constructor

--- src/main/java/model/Stadium.java
+++ src/main/java/model/Stadium.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Stadium {
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Stadium extends BaseEntity <Integer>{
+    String stadiumName;
+    int cityId;
+
+    public  Stadium (int id, String stadiumName, int cityId){
+        super(id);
+        this.stadiumName=stadiumName;
+        this.cityId=cityId;
+    }
 }
