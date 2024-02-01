d4160f484607a2239829d08ccf59fc7253d0203b
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-01 16:17:59 +0330

    creat table player

--- src/main/java/sql/tables.sql
+++ src/main/java/sql/tables.sql
@@ -10,4 +10,11 @@ create table if not exists team
   team_name varchar(50),
   city_id int references city (id)
 );
+CREATE TABLE IF NOT EXISTS player
+(
+    id SERIAL PRIMARY KEY ,
+    player_name VARCHAR(50),
+    salary DECIMAL(10,2) ,
+    team_id int references team(id)
+);
 
