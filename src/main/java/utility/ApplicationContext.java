2ecbc3f827c796429d148952c583ddb10d65e85b
samangishamim <samangi624@gmail.com>
2024-02-01 11:48:10 +0330

    add connection to all classes for database

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,4 +1,29 @@
 package utility;
 
+import connection.DBConfig;
+import repository.city.CityRepository;
+import repository.city.CityRepositoryImpl;
+import service.city.CityService;
+import service.city.CityServiceImpl;
+
+import java.sql.Connection;
+
 public class ApplicationContext {
+    private  static Connection CONNECTION;
+    private  static CityRepository CITY_REPOSITORY;
+    private  static CityService CITY_SERVICE;
+
+    public static void initialize(){
+        if (CONNECTION == null){
+            CONNECTION= DBConfig.getConnection();
+        }
+        if (CITY_REPOSITORY == null){
+            CITY_REPOSITORY= new CityRepositoryImpl(CONNECTION);
+            CITY_SERVICE=new CityServiceImpl(CITY_REPOSITORY);
+        }
+    }
+
+    public  static  CityService getCityService(){
+        return CITY_SERVICE;
+    }
 }
