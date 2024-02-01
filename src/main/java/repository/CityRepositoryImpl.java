428e38ffa1ace8def43fc844968e277070305731
samangishamim <samangi624@gmail.com>
2024-02-01 11:15:39 +0330

    implement all methods and constructor

--- src/main/java/repository/CityRepositoryImpl.java
+++ src/main/java/repository/CityRepositoryImpl.java
@@ -1,4 +1,44 @@
 package repository;
 
-public class CityRepositoryImpl {
+import base.repository.BaseRepositoryImpl;
+import model.City;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class CityRepositoryImpl extends BaseRepositoryImpl <Integer, City> implements CityRepository {
+    protected CityRepositoryImpl(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public String getQuestionMarks() {
+        return null;
+    }
+
+    @Override
+    public String getFieldName() {
+        return null;
+    }
+
+    @Override
+    public String getUpdateFields() {
+        return null;
+    }
+
+    @Override
+    public String setFields(PreparedStatement ps, City entity, boolean isCountOnly) {
+        return null;
+    }
+
+    @Override
+    public City mapResultSetToEntity(ResultSet resultSet) {
+        return null;
+    }
 }
