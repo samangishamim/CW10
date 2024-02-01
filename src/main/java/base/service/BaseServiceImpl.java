3649d28efe4bec39d5df99be6f84f5dd30bccacc
samangishamim <samangi624@gmail.com>
2024-02-01 10:16:51 +0330

    implement methods and  constructor for repository

--- src/main/java/base/service/BaseServiceImpl.java
+++ src/main/java/base/service/BaseServiceImpl.java
@@ -1,4 +1,39 @@
 package base.service;
 
-public class BaseServiceImpl {
+import base.model.BaseEntity;
+import base.repository.BaseRepository;
+
+import java.io.Serializable;
+
+public class BaseServiceImpl <
+        ID extends Serializable,
+        T extends BaseEntity<ID>,
+        R extends BaseRepository<ID,T>
+        > implements BaseService<ID , T> {
+
+    public  final R repository;
+
+    public BaseServiceImpl(R repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void save(T entity) {
+
+    }
+
+    @Override
+    public T findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public void update(T entity) {
+
+    }
+
+    @Override
+    public void delete(ID id) {
+
+    }
 }
