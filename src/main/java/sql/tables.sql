0da72f70ee316955edd6539e9d8ec0e5bd4c0947
samangishamim <samangi624@gmail.com>
2024-02-01 14:18:02 +0330

    create team table

--- src/main/java/sql/tables.sql
+++ src/main/java/sql/tables.sql
@@ -4,3 +4,10 @@ create  table  if not exists  city
     city_name varchar(50)
 );
 
+create table if not exists team
+(
+  id SERIAL PRIMARY KEY ,
+  team_name varchar(50),
+  city_id int references city (id)
+);
+
