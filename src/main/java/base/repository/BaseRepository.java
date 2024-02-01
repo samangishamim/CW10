f01753d93b16711af27b0552e25b176f507b8a1f
samangishamim <samangi624@gmail.com>
2024-02-01 09:30:33 +0330

    add methods

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -1,4 +1,15 @@
 package base.repository;
 
-public interface BaseRepository {
+import base.model.BaseEntity;
+
+import java.io.Serializable;
+
+public interface BaseRepository<ID extends Serializable, T extends BaseEntity<ID>> {
+    void save(T entity);
+
+    T findById(ID id);
+
+    void update(T entity);
+
+    void delete(ID id);
 }
