b1b1a0c85b19b45c8ee58a2be792f80243f22f31
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-02 15:03:41 +0330

    add needed fields and needed constructor

--- src/main/java/model/Coach.java
+++ src/main/java/model/Coach.java
@@ -1,4 +1,25 @@
 package model;
 
-public class Coach {
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Coach extends BaseEntity<Integer> {
+    String coachName;
+    int teamId;
+    int contractId;
+
+    public Coach (int id , String coachName , int teamId , int contractId){
+        super(id);
+        this.coachName=coachName;
+        this.teamId=teamId;
+        this.contractId=contractId;
+    }
 }
+
