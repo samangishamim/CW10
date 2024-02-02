603a0e53c04ae78d55ca57dff5c17382340d91e4
arianaaghajani <aghajaniariana@gmail.com>
2024-02-02 10:03:50 +0330

    extends BaseRepository

--- src/main/java/repository/match/MatchRepository.java
+++ src/main/java/repository/match/MatchRepository.java
@@ -1,4 +1,7 @@
 package repository.match;
 
-public interface MatchRepository {
+import base.repository.BaseRepository;
+import model.Match;
+
+public interface MatchRepository extends BaseRepository<Integer, Match> {
 }
