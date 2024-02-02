91c0ad1203d07992ac7c1059e017e70ca2e08791
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-02 11:55:18 +0330

    implement all methods

--- src/main/java/repository/contract/ContractRepositoryImpl.java
+++ src/main/java/repository/contract/ContractRepositoryImpl.java
@@ -1,4 +1,51 @@
 package repository.contract;
 
-public class ContractRepositoryImpl {
+import base.repository.BaseRepositoryImpl;
+import model.Contract;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class ContractRepositoryImpl extends BaseRepositoryImpl<Integer , Contract>
+        implements ContractRepository {
+    public ContractRepositoryImpl(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public String getTableName() {
+        return "contract";
+    }
+
+    @Override
+    public String getFieldName() {
+        return "(start , finish , salary)";
+    }
+
+    @Override
+    public String getQuestionMarks() {
+        return "(?,?,?)";
+    }
+
+    @Override
+    public String getUpdateFields() {
+        return "start = ? , finish = ? , salary = ?";
+    }
+
+    @Override
+    public void setFields(PreparedStatement ps, Contract entity, boolean isCountOnly) throws SQLException {
+        ps.setDate(1,entity.getStart());
+        ps.setDate(2,entity.getFinish());
+        ps.setBigDecimal(3,entity.getSalary());
+    }
+
+    @Override
+    public Contract mapResultSetToEntity(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt(1);
+        Date start = resultSet.getDate(2);
+        Date finish = resultSet.getDate(3);
+        BigDecimal salary = resultSet.getBigDecimal(4);
+
+        return new Contract(id , start , finish , salary);
+    }
 }
