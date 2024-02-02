01afee2250af97055b9ada14c15dfb90587c4ef9
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-02 15:18:03 +0330

    add constructor

--- src/main/java/service/coach/CoachServiceImpl.java
+++ src/main/java/service/coach/CoachServiceImpl.java
@@ -1,4 +1,12 @@
 package service.coach;
 
-public class CoachServiceImpl {
+import base.service.BaseServiceImpl;
+import model.Coach;
+import repository.coach.CoachRepository;
+
+public class CoachServiceImpl extends BaseServiceImpl<Integer , Coach , CoachRepository>
+        implements CoachService {
+    public CoachServiceImpl(CoachRepository repository) {
+        super(repository);
+    }
 }
