099abaa820230378e0bfc64fe6a58eb191bd8693
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-02 11:44:48 +0330

    add needed fields and needed constructor

--- src/main/java/model/Contract.java
+++ src/main/java/model/Contract.java
@@ -1,4 +1,27 @@
 package model;
 
-public class Contract {
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Contract extends BaseEntity<Integer> {
+    Date start ;
+    Date finish ;
+    BigDecimal salary ;
+
+    public Contract(int id, Date start, Date finish, BigDecimal salary) {
+        super(id);
+        this.start=start;
+        this.finish=finish;
+        this.salary=salary;
+    }
 }
