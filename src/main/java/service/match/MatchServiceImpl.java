0c21c411ba35db5951b044f66df90bcaa8461984
arianaaghajani <aghajaniariana@gmail.com>
2024-02-02 10:27:51 +0330

    add constructor

--- src/main/java/service/match/MatchServiceImpl.java
+++ src/main/java/service/match/MatchServiceImpl.java
@@ -1,4 +1,12 @@
 package service.match;
 
-public class MatchServiceImpl {
+import base.service.BaseServiceImpl;
+import model.Match;
+import repository.match.MatchRepository;
+
+public class MatchServiceImpl extends BaseServiceImpl<Integer, Match, MatchRepository>
+        implements MatchService{
+    public MatchServiceImpl(MatchRepository repository) {
+        super(repository);
+    }
 }
