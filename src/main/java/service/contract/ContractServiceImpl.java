26ab89af93ab6c60a0754b7313eda2d16e7b20fa
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-02 11:59:14 +0330

    add constructor

--- src/main/java/service/contract/ContractServiceImpl.java
+++ src/main/java/service/contract/ContractServiceImpl.java
@@ -1,4 +1,12 @@
 package service.contract;
 
-public class ContractServiceImpl {
+import base.service.BaseServiceImpl;
+import model.Contract;
+import repository.contract.ContractRepository;
+
+public class ContractServiceImpl extends BaseServiceImpl <Integer , Contract , ContractRepository>
+        implements ContractService {
+    public ContractServiceImpl(ContractRepository repository) {
+        super(repository);
+    }
 }
