89bba673b656596df467c6283914c1b57ed2360a
arianaaghajani <aghajaniariana@gmail.com>
2024-02-02 09:57:30 +0330

    add lombok annotation and constructor

--- src/main/java/model/Match.java
+++ src/main/java/model/Match.java
@@ -1,4 +1,27 @@
 package model;
 
-public class Match {
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Match extends BaseEntity<Integer> {
+    int numberOfGoals;
+    int score;
+    int teamId;
+    int stadiumId;
+
+
+    public Match(int id,int numberOfGoals,int score,int teamId, int stadiumId){
+        super(id);
+        this.numberOfGoals=numberOfGoals;
+        this.score=score;
+        this.teamId=teamId;
+        this.stadiumId=stadiumId;
+    }
 }
